a04465eae739b26f705fc3b286242badcd9119d1
WIN-5O9C6DPTQRJ\ICT02_05 <pongdang123@gmail.com>
2024-07-17 17:58:47 +0900

    7.17 마무리

--- src/CharMain.java
+++ src/CharMain.java
@@ -2,39 +2,36 @@
 public class CharMain {
 
 	public static void main(String[] args) {
-		char ch11=' ';
-        int a= 1;
+		char ch11 = ' ';
+		int a = 1;
 
-        System.out.println(ch11 + "똠다");
-        System.out.println(a + "나다");
-        
-        
-        char ch13 = 'A';
-        char ch14 = 65;
-        
-        System.out.println(ch13);
-        System.out.println(ch14);
-        
-        char ch15 = '1';
-        char ch16 = 49;
-        
-        System.out.println(ch15);
-        System.out.println(ch16);
-        
-        
+		System.out.println(ch11 + "똠다");
+		System.out.println(a + "나다");
 
-            // char ch1 = '��';
-            char ch2 = 'Ȯ';
+		char ch13 = 'A';
+		char ch14 = 65;
 
-            char ch3 = 54736;
-            char ch4 = 54869;
-        
-            char ch5 = 0xD5D0;
-            char ch6 = 0xD655;
-        
-            // System.out.println(ch1 + " " + ch2);
-            System.out.println(ch3 + " " + ch4);
-            System.out.println(ch5 + " " + ch6);     
+		System.out.println(ch13);
+		System.out.println(ch14);
+
+		char ch15 = '1';
+		char ch16 = 49;
+
+		System.out.println(ch15);
+		System.out.println(ch16);
+
+		// char ch1 = '��';
+		char ch2 = 'Ȯ';
+
+		char ch3 = 54736;
+		char ch4 = 54869;
+
+		char ch5 = 0xD5D0;
+		char ch6 = 0xD655;
+
+		// System.out.println(ch1 + " " + ch2);
+		System.out.println(ch3 + " " + ch4);
+		System.out.println(ch5 + " " + ch6);
 
 	}
 
--- src/NineNine.java
+++ src/NineNine.java
@@ -5,30 +5,31 @@ public class NineNine {
 		// TODO Auto-generated method stub
 		// 구구단
 
-				int x = 0;
-				int y = 0;
-				int xy = 0;
-				for (x=2;x<=9;x++) {
-					System.out.println("================================");
-					System.out.println(x + "단을 출력합니다");
-					//if (x % 2 != 0) continue;
-					for (y=1;y<=9;y++) {
-						xy = x * y;
-						if (xy % 2 == 0) continue;
-						System.out.println(x + " X " + y + " = " + xy );
-					}
-				}
+		int x = 0;
+		int y = 0;
+		int xy = 0;
+		for (x = 2; x <= 9; x++) {
+			System.out.println("================================");
+			System.out.println(x + "단을 출력합니다");
+			// if (x % 2 != 0) continue;
+			for (y = 1; y <= 9; y++) {
+				xy = x * y;
+				if (xy % 2 == 0)
+					continue;
+				System.out.println(x + " X " + y + " = " + xy);
+			}
+		}
 
-				System.out.println("================================");
-				System.out.println("================================");
+		System.out.println("================================");
+		System.out.println("================================");
 
-				int num9 = 0;
-				while (num9 < 9) {
-					++num9;
-					System.out.println("9 X " + num9 + " = " + 9 * num9 );
-					
-				}
-				
+		int num9 = 0;
+		while (num9 < 9) {
+			++num9;
+			System.out.println("9 X " + num9 + " = " + 9 * num9);
+
+		}
+
 	}
 
 }
